00f51dc83681481bc0d790d926e12a46cffed22b
vividh.kumar <vividh.kumar@swiggy.in>
2022-07-08 20:34:59 +0530

    change contracts

--- demo/src/main/java/com/example/demo/Share.java
+++ demo/src/main/java/com/example/demo/Share.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 public class Share {
 	public String name;
-	public String vpa;
 	public String email;
 	public int amount;
 }
--- demo/src/main/java/com/example/demo/Split.java
+++ demo/src/main/java/com/example/demo/Split.java
@@ -5,5 +5,6 @@ import java.util.ArrayList;
 public class Split {
 	public String emaiId;
 	public String name;
+	public String vpa;
 	public ArrayList<Share> shares ;
 }
